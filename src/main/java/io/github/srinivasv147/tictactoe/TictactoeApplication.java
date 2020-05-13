17ccc741ae68aedd0efc74d8b3648c02c3bba2ba
Srinivas Vaidyanathan <vaidyanathanrathka@gmail.com>
2020-05-14 01:38:42 +0530

    added initial files to project

---
+++ src/main/java/io/github/srinivasv147/tictactoe/TictactoeApplication.java
@@ -0,0 +1,13 @@
+package io.github.srinivasv147.tictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/srinivasv147/tictactoe/TictactoeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.srinivasv147.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
